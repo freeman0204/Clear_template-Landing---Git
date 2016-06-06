17c5f35920b2cd9c261b58f105815dd9da2f8325
Artur <artrur0204@gmail.com>
2016-06-06 21:09:02 +0300

    init

---
+++ js/common.js
@@ -0,0 +1,12 @@
+//Адаптивное меню
+$(document).ready(function () {
+    $('.menu-trigger').click(function () {
+        $('nav ul').slideToggle(500);
+    }); //end slide toggle
+
+    $(window).resize(function () {
+        if ($(window).width() > 600) {
+            $('nav ul').removeAttr('style');
+        }
+    }); //end resize
+}); //end ready
